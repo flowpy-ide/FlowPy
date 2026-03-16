cfc634998ddc20ea1a07b3b30cdb225d48f42f74
Erkan3034 <turguterkan55@gmail.com>
2026-03-16 15:31:48 +0300

    feat: Add new example flow definitions with conditional logic and their corresponding Python export.

---
+++ flow_export.py
@@ -0,0 +1,12 @@
+# FlowPy Tarafından Üretilen Python Kodu
+
+def main():
+    x = input('Bir sayı girin: ')
+    result = x*5
+    if result>5:
+        print("Kosul dogru")
+    else:
+        print("hata")
+
+if __name__ == '__main__':
+    main()
